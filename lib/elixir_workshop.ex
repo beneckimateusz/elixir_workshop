4a8f7e709c60d9c14742925b3aaf6e94165b157d
Michal Sewczyk <michal.szewczyk@appliscale.io>
2023-10-05 15:46:18 +0200

    Add missing handlers

--- lib/elixir_workshop.ex
+++ lib/elixir_workshop.ex
@@ -5,18 +5,67 @@ defmodule ElixirWorkshop do
 
   @impl true
   def init(attrs, ctx) do
-    {:ok, ctx, editor: [attribute: "code", language: "elixir"]}
+    source = attrs["source"] || ""
+    {:ok, assign(ctx, source: source), editor: [attribute: "code", language: "elixir"]}
   end
 
-  # Other Kino.JS.Live callbacks
+  @impl true
+  def handle_connect(ctx) do
+    {:ok, %{source: ctx.assigns.source}, ctx}
+  end
+
+  @impl true
+  def handle_event("update", %{"source" => source}, ctx) do
+    broadcast_event(ctx, "update", %{"source" => source})
+    {:noreply, assign(ctx, source: source)}
+  end
 
   @impl true
   def to_attrs(ctx) do
-    %{}
+    %{"source" => ctx.assigns.source}
   end
 
   @impl true
   def to_source(attrs) do
-    nil
+    attrs["source"]
+  end
+
+  asset "main.js" do
+    """
+    export function init(ctx, payload) {
+      ctx.importCSS("main.css");
+
+      ctx.root.innerHTML = `
+        <textarea id="source"></textarea>
+      `;
+
+      const textarea = ctx.root.querySelector("#source");
+      textarea.value = payload.source;
+
+      textarea.addEventListener("change", (event) => {
+        ctx.pushEvent("update", { source: event.target.value });
+      });
+
+      ctx.handleEvent("update", ({ source }) => {
+        textarea.value = source;
+      });
+
+      ctx.handleSync(() => {
+        // Synchronously invokes change listeners
+        document.activeElement &&
+          document.activeElement.dispatchEvent(new Event("change"));
+      });
+    }
+    """
+  end
+
+  asset "main.css" do
+    """
+    #source {
+      box-sizing: border-box;
+      width: 100%;
+      min-height: 100px;
+    }
+    """
   end
 end
